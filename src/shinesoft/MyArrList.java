a73d15362bee9d1b6dd1e0637ef84cf13e45c6b9
olexy <h.lekan@gmail.com>
2020-10-28 23:39:51 -0500

    array list demo on master branch

---
+++ src/shinesoft/MyArrList.java
@@ -0,0 +1,19 @@
+package shinesoft;
+
+import java.util.*;
+
+public class MyArrList {
+	
+	public static void main(String[] args) {
+		String st[] = new String[5];
+		ArrayList al1  = new ArrayList();
+		ArrayList al2 = new ArrayList(5);
+		for(String s:st) 
+		{
+			System.out.println(s);
+		}
+		
+		System.out.println(al1);
+		System.out.println(al1);
+	}
+}
